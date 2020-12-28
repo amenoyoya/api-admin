d13892288d3df6abe4249ef0dabe2906637a1bba
amenoyoya <mtyswe3@gmail.com>
2020-12-29 03:13:55 +0900

    👽 test: codeceptjs: link全取得

--- codecept/_test.js
+++ codecept/_test.js
@@ -1,6 +1,9 @@
 Feature('My First Test');
 
-Scenario('test something', ({ I }) => {
+Scenario('test something', async ({ I }) => {
   I.amOnPage('https://github.com');
+  // GitHub の文字列があるか確認
   I.see('GitHub');
+  // link 全取得
+  console.log(await I.grabAttributeFromAll('a', 'href'));
 });
